edf5dc0a94340ca655783a261b38f8918aae3b3c
ainhoa <apedrosa@elpuig.xeill.net>
2025-12-02 17:48:30 +0100

    Tabla de country con id, code y name

---
+++ src/main/java/com/example/conflict_tracker/domain/Country.java
@@ -0,0 +1,40 @@
+package com.example.conflict_tracker.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
